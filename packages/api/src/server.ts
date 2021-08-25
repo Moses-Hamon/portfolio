d3516b724ee05eeca4dc931ba8524cfece5d2821
Moses Hamon <moseslhamon@gmail.com>
2021-08-25 21:03:01 +1000

    add tailwind

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -3,6 +3,14 @@ import express from "express";
 const app = express();
 const port = process.env.API_PORT || 4000;
 
+app.use((req, res, next) => {
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("ok");
+});
+
 app.listen(port, () => {
   console.info(`App listening on port: ${port}`);
 });
